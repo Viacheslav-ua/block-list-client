e565a66e1cf7184bc2b683395dff58ea7d05c789
Viacheslav-ua <atph@ukr.net>
2023-12-21 12:18:50 +0200

    UiButton added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { authControllerGetSessionInfo } from '@/shared/api/generated'
 import { useQuery } from '@tanstack/react-query'
+import { UiButton } from '@/shared/ui/ui-button'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,10 @@ export default function HomePage() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 `}
     >
       <h1>Work {data?.email}</h1>
+      <UiButton variant="primary">Hey</UiButton>
+      <UiButton variant="secondary">Sign Out</UiButton>
+      <UiButton variant="outlined">Sign Out</UiButton>
+      <UiButton disabled variant="primary">Hay</UiButton>
     </main>
   )
 }
